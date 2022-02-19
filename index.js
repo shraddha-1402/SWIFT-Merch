0f557965ab09bf59b898c133020ddbc02392ef26
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-02-19 13:27:35 +0530

    feat: added jsdoc in index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-let slideIndex = 0;
-let imgSrc = [
-  "/Assets/HeroImages/evermore-carousel-img.png",
-  "/Assets/HeroImages/fearless-carousel-img.png",
-  "/Assets/HeroImages/red-carousel-img.png"
-];
+/**
+ * Carousel for mobile hero section
+ * Changes hero image src after 2 secs
+ * @param {number} slideIndex - number of image index to be displayed
+ * @param {array of string} imgSrc - array of sources of hero images
+ */
 
-const changeSlides = () => {
+const changeSlides = (slideIndex, imgSrc) => {
   let slide = document.querySelector("#slides");
   slide.src = imgSrc[slideIndex%imgSrc.length];
   slideIndex++;
-  setTimeout(changeSlides, 2000);
+  setTimeout(changeSlides, 2000, slideIndex, imgSrc);
 }
 
-changeSlides();
+changeSlides(0, [
+  "/Assets/HeroImages/evermore-carousel-img.png",
+  "/Assets/HeroImages/fearless-carousel-img.png",
+  "/Assets/HeroImages/red-carousel-img.png"
+]);
